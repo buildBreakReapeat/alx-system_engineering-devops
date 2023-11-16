330d84ae8a109dba9c3555876b80f503173de338
jay-alx <126336909+younis-alx@users.noreply.github.com>
2023-11-16 14:46:00 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,51 +1,12 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+# Fix high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# Stop and disable the default Nginx service
-service { 'nginx':
-  ensure => stopped,
-  enable => false,
-}
-
-# Configure Nginx
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define Nginx virtual host configuration
-file { '/etc/nginx/conf.d/my_website.conf':
-  ensure  => file,
-  content => template('nginx/my_website.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx service
-exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/etc/init.d/',
-  require => Exec['fix--for-nginx'],
-  notify  => Service['nginx'],
-}
-
-# Start Nginx service
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-  subscribe  => [File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/my_website.conf'], Exec['nginx-restart']],
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
